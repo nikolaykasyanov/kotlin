e243a0ff23121c9c0d04b819ea5356cfae62c82b
Ilya Goncharov <ilya.goncharov@jetbrains.com>
2021-10-29 15:03:13 +0300

    [JS IR] Use stdlib's listOfNotNull

--- compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelToJsStatements.kt
+++ compiler/ir/backend.js/src/org/jetbrains/kotlin/ir/backend/js/export/ExportModelToJsStatements.kt
@@ -131,7 +131,7 @@ class ExportModelToJsStatements(
                 val staticsExport = (staticFunctions + staticProperties + declaration.nestedClasses)
                     .flatMap { generateDeclarationExport(it, newNameSpace, esModules) }
 
-                (klassExport?.let { listOf(it) } ?: emptyList()) + staticsExport + innerClassesAssignments
+                listOfNotNull(klassExport) + staticsExport + innerClassesAssignments
             }
         }
     }
